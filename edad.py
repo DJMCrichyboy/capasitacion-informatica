4c595ba02ca1a94663ca9c1b3f0201a01faf91db
Ricardo MOrales <rmc150@gmail.com>
2016-09-09 10:09:04 -0700

    primer programa de capacitacion

---
+++ edad.py
@@ -0,0 +1,10 @@
+print ("hola usuario")
+nombre1 = imput ("dime tu nombre:")
+edad1 = int(input("dime tu edad: ")
+nombre2 = imput ("dime otro nombre:")
+edad2 = int(input("dime tu edad:")
+
+
+
+
+ 
